17df0a55f41d8087a041b36002bec59695d77a78
clurdish <clurdish@gmail.com>
2016-01-20 23:15:39 -0600

    Edit Test1.java
    Actually edit the code in netbeans

--- Test1.java
+++ Test1.java
@@ -1,11 +1,23 @@
 // This is a new file
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class Test1
 {
-  public static void main(String [] args)
-  {
-    System.out.println("How does this work?");
-  }
+    public static void main(String [] args)
+    {
+        System.out.println("How does this work?");
+        System.out.println("Maybe someday I'll figure it out");
+        
+        String [] unnecessary = new String [10];
+        Random rand = new Random();
+        
+        for(int i=0; i<10; i++)
+        {
+            unnecessary[i] = Integer.toString(rand.nextInt());
+        }
+        
+        for(String s : unnecessary)
+            System.out.println(s);
+    }
 }
